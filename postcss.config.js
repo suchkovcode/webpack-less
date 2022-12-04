14c3e35382e1dfbb548334cee59a5f9063f9a03a
Nikita Suchkov <suchkov8520@gmail.com>
2022-12-04 17:40:36 +0300

    Git init

---
+++ postcss.config.js
@@ -0,0 +1,32 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+   plugins: [
+      require("postcss-100vh-fix"),
+      isDev ? undefined : require("autoprefixer"),
+      isDev ? undefined : require("postcss-mq-keyframes"),
+      isDev ? undefined : require("postcss-sort-media-queries"),
+      isDev ? undefined : require("postcss-focus"),
+      purgecss({
+         content: [],
+         css: [],
+         skippedContentGlobs: ["node_modules/**"],
+         dynamicAttributes: [""],
+         fontFace: false,
+         keyframes: false,
+         variables: true,
+         rejected: false,
+         rejectedCss: false,
+         safelist: {
+            standard: [],
+            deep: [],
+            greedy: [],
+            keyframes: [],
+            variables: []
+         },
+      })
+   ],
+};
+
+
